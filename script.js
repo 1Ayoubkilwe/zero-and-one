702ffe5fd3ad514350ddf4b296b81abb5b87e655
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-08 16:50:44 +0300

    answers

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+function ZeroAndOne(arr) {
+    return arr
+      .map(item => {
+        if (item === 'Zero') return '0';
+        if (item === 'One') return '1';
+        return ''; // Ignore other values
+      })
+      .join('');
+  }
+  
+
+  console.log(ZeroAndOne(["one", "Zero", "Zero", "One"])); // "001"
+  console.log(ZeroAndOne(["Zero", "ONE", "one", "Zero"])); // "00"
+  
